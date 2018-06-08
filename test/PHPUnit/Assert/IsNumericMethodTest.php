3de3797318d2d65aa9dbbc47a5288bcbeba36410
Necip Allef <necipallef@gmail.com>
2018-06-08 16:45:53 +0300

    ADD isnumericmethodtest

---
+++ test/PHPUnit/Assert/IsNumericMethodTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Fabstract\Component\Assert\Test\PHPUnit\Assert;
+
+use Fabstract\Component\Assert\Assert;
+use Fabstract\Component\Assert\AssertionException;
+use Fabstract\Component\Assert\Test\PHPUnit\MethodTestBase;
+
+class IsNumericMethodTest extends MethodTestBase
+{
+
+    #region correct arguments
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testFloatOneDoesNotThrow()
+    {
+        $argument = [1.0];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testFloatZeroDoesNotThrow()
+    {
+        $argument = [0.0];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testINFDoesNotThrow()
+    {
+        $argument = [INF];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testIntOneDoesNotThrow()
+    {
+        $argument = [1];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testIntZeroDoesNotThrow()
+    {
+        $argument = [0];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testIntNegativeOneDoesNotThrow()
+    {
+        $argument = [-1];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testPHPIntMaxDoesNotThrow()
+    {
+        $argument = [PHP_INT_MAX];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testPHPIntMinDoesNotThrow()
+    {
+        $argument = [PHP_INT_MIN];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testStringZeroDoesNotThrow()
+    {
+        $argument = ['0'];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    #endregion
+
+    #region incorrect arguments
+
+    public function testNullThrows()
+    {
+        $argument = [null];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    #endregion
+}
