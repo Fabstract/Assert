c3badb93c9bad0723d0168c7c804eb32ff73c09c
Necip Allef <necipallef@gmail.com>
2018-09-10 10:50:57 +0300

    ADD int operation tests

---
+++ test/PHPUnit/Assert/IsNotNegativeIntMethodTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Fabstract\Component\Assert\Test\PHPUnit\Assert;
+
+use Fabstract\Component\Assert\Assert;
+use Fabstract\Component\Assert\AssertionException;
+use Fabstract\Component\Assert\Test\PHPUnit\MethodTestBase;
+
+class IsNotNegativeIntMethodTest extends MethodTestBase
+{
+
+    #region correct arguments
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testPositiveOneDoesNotThrow()
+    {
+        $argument = [1];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testZeroDoesNotThrow()
+    {
+        $argument = [0];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    #endregion
+
+    #region incorrect arguments
+
+    public function testNullThrows()
+    {
+        $argument = [null];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testNegativeOneThrows()
+    {
+        $argument = [-1];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testFloatPositiveOneThrows()
+    {
+        $argument = [1.0];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testPositiveInfinityThrows()
+    {
+        $argument = [INF];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testNonIntThrows()
+    {
+        $argument = ['1'];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    #endregion
+}
---
+++ test/PHPUnit/Assert/IsPositiveIntMethodTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Fabstract\Component\Assert\Test\PHPUnit\Assert;
+
+use Fabstract\Component\Assert\Assert;
+use Fabstract\Component\Assert\AssertionException;
+use Fabstract\Component\Assert\Test\PHPUnit\MethodTestBase;
+
+class IsPositiveIntMethodTest extends MethodTestBase
+{
+
+    #region correct arguments
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testPositiveOneDoesNotThrow()
+    {
+        $argument = [1];
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    #endregion
+
+    #region incorrect arguments
+
+    public function testNullThrows()
+    {
+        $argument = [null];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testNegativeOneThrows()
+    {
+        $argument = [-1];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testFloatPositiveOneThrows()
+    {
+        $argument = [1.0];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testZeroThrows()
+    {
+        $argument = [0];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testPositiveInfinityThrows()
+    {
+        $argument = [INF];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    public function testNonIntThrows()
+    {
+        $argument = ['1'];
+
+        $this->expectException(AssertionException::class);
+
+        $this->callStatic(Assert::class, $argument);
+    }
+
+    #endregion
+}
